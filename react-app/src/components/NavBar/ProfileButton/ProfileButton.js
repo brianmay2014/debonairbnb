e155dc90afd3e30c116380b6896bc24598fd3d59
Paul-Eirik <pmelhus@gmail.com>
2022-05-27 13:07:40 -0400

    added profile button dropdown menu

--- react-app/src/components/NavBar/ProfileButton/ProfileButton.js
+++ react-app/src/components/NavBar/ProfileButton/ProfileButton.js
@@ -1,34 +1,65 @@
-import LogoutButton from '../../auth/LogoutButton';
+import LogoutButton from "../../auth/LogoutButton";
+import React, { useState, useEffect } from "react";
 import { Redirect, useHistory, NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import "./ProfileButton.css"
 
 const ProfileButton = () => {
-  return (
-    <ul>
-    <li>
-      <NavLink to='/' exact={true} activeClassName='active'>
-        Home
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to='/login' exact={true} activeClassName='active'>
-        Login
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to='/sign-up' exact={true} activeClassName='active'>
-        Sign Up
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to='/users' exact={true} activeClassName='active'>
-        Users
-      </NavLink>
-    </li>
-    <li>
-      <LogoutButton />
-    </li>
-  </ul>
-  )
-}
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
 
-export default ProfileButton
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  return (
+    <div>
+      <button className="menu-button" onClick={openMenu}>
+        <i class="fa-solid fa-bars"></i>
+      </button>
+
+      {showMenu && (
+        <ul className='profile-dropdown-menu'>
+          <li>
+            <NavLink to="/" exact={true} activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" exact={true} activeClassName="active">
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/sign-up" exact={true} activeClassName="active">
+              Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/users" exact={true} activeClassName="active">
+              Users
+            </NavLink>
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ProfileButton;
