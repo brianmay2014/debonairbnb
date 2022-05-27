6941b4e8359dbb6c1b461c7348c47bbad891149f
Brian May <brianmay2014@gmail.com>
2022-05-27 11:13:32 -0700

    date objects are loading in react form

--- react-app/src/components/estate/DescriptionAvailCharter.js
+++ react-app/src/components/estate/DescriptionAvailCharter.js
@@ -1,9 +1,8 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Route, Redirect, useParams } from "react-router-dom";
-import './estatePage.css';
-import ImageDisplay from "./ImageDisplay";
-import DescriptionAvailCharter from "./DescriptionAvailCharter";
+import "./estatePage.css";
+import CharterForm from "./CharterForm";
 
 const estate = {
 	address: "10922 Corbly Gulch Rd, Belgrade, MT 59714",
@@ -15,7 +14,8 @@ const estate = {
 		"Don’t miss the opportunity to stay at this unique cabin getaway! This private and cozy home books up fast with the unparalleled 360 views and outstanding accommodations on 100 acres featuring premium bedding, extensive new renovations, and comfortable entertainment space. The home is perfect for an intimate getaway or group gathering! Ideal location for access to trails minutes away, 15 mins to the outskirts of Bozeman, and surrounded by local event venues.",
 };
 
-const EstatePage = () => {
+
+const DescriptionAvailCharter = () => {
 	const { id } = useParams();
 
 	// const dispatch = useDispatch();
@@ -31,31 +31,32 @@ const EstatePage = () => {
 
 	const { address, owner_id, title, nightly_rate, type_id, description } =
 		estate;
-    const avg_rating = 4.5
-    const rating_count = 15
+    
+    const ownerName = "Jerrey";
+    const numGuests = 6;
+    const numBaths = 3;
+    const numBeds =  6;
+
 
 	return (
-		<div id='estate-body'>
-			<div className='estate-head-info'>
-                <div id='estate-head-title'>
-                    {title}
+		<div id="desc-avail-charter">
+            <div id='dac-left'>
+                <div id='description-header'>
+                    {title} hosted by {ownerName}
                 </div>
-                <div id='estate-head-info'>
-                    <div>
-                        {avg_rating} stars - {rating_count} critiques
-                    </div>
-                    <div>
-                        {address}
-                    </div>
-
+                <div id='estate-details'>
+                    {numGuests} guests - {numBeds} beds - {numBaths} baths
+                </div>
+                <div id='estate-description'>
+                    {description}
                 </div>
             </div>
-
-            <ImageDisplay />
-            <DescriptionAvailCharter />   
-           
+            <div id='dac-right'>
+                <CharterForm />
+            </div>
 		</div>
 	);
 };
 
-export default EstatePage;
+export default DescriptionAvailCharter;
+
