3ccb2a5d4f8b540807d43042539731ccaebe365b
Brian May <brianmay2014@gmail.com>
2022-05-27 12:47:58 -0700

    charter form built out

--- react-app/src/components/estate/CharterForm.js
+++ react-app/src/components/estate/CharterForm.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Route, Redirect, useParams } from "react-router-dom";
 import "./estatePage.css";
-import { getCheckinDate, getCheckoutDate } from "./utils";
+import { getCheckinDate, getCheckoutDate, getNightStay } from "./utils";
 
 const estate = {
 	address: "10922 Corbly Gulch Rd, Belgrade, MT 59714",
@@ -17,10 +17,13 @@ const estate = {
 const CharterForm = () => {
 	const { id } = useParams();
 
-    
+	const [checkinDate, setCheckinDate] = useState(getCheckinDate());
+	const [checkoutDate, setCheckoutDate] = useState(getCheckoutDate());
+	const [nightStay, setNightStay] = useState(getNightStay());
 
-    const [checkinDate, setCheckinDate] = useState(getCheckinDate());
-    const [checkoutDate, setCheckoutDate] = useState(getCheckoutDate());
+    //cleaning rate, can set up fancy randomizer, or just leave it as 15% of one night's stay
+    const cleanRate = 0.15;
+
 	// const dispatch = useDispatch();
 
 	// once state is constructed
@@ -32,17 +35,21 @@ const CharterForm = () => {
 	//     dispatchEvent(getEstate(id));
 	// }, [dispatch]);
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+	};
 
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-
-
-    }
-
-    const errors = [];
+	const errors = [];
 
 	const { address, owner_id, title, nightly_rate, type_id, description } =
 		estate;
+
+
+    const baseCost = nightStay * nightly_rate;
+    const cleanCost = nightly_rate * cleanRate;
+    const servCost = 0;
+    const totalCost = baseCost + cleanCost + servCost;
+
 
 	return (
 		<div id="charter-form">
@@ -53,25 +60,55 @@ const CharterForm = () => {
 					))}
 				</ul>
 				<p>${nightly_rate} / night</p>
-				<label>Check-in</label>
-				<input
-					type="date"
-					value={checkinDate}
-					required
-					onChange={(e) => setCheckinDate(e.target.value)}
-				/>
-				<label>Check-out</label>
-				<input
-					type="date"
-					value={checkoutDate}
-					required
-					onChange={(e) => setCheckoutDate(e.target.value)}
-				/>
-                <button className='btn' id='reserve-btn' type='submit'>
-                    Reserve
-                </button>
+				<div className="checkdates">
+					<label>Check-in</label>
+					<input
+						type="date"
+						value={checkinDate}
+						required
+						onChange={(e) => setCheckinDate(e.target.value)}
+					/>
+				</div>
+				<div className="checkdates">
+					<label>Check-out</label>
+					<input
+						type="date"
+						value={checkoutDate}
+						required
+						onChange={(e) => setCheckoutDate(e.target.value)}
+					/>
+				</div>
+				<button className="btn" id="reserve-btn" type="submit">
+					Reserve
+				</button>
 			</form>
-            You won't be charged yet
+			<p id='no-charge-yet'>You won't be charged yet</p>
+			<div id="charter-invoice">
+				<div className='invoice-row' id="base-cost">
+					<p className="invoice-left">
+						${nightly_rate} x {nightStay} nights
+					</p>
+					<p className="invoice-right">${baseCost}</p>
+				</div>
+				<div className='invoice-row' id="clean-cost">
+					<p className="invoice-left">
+						Cleaning fee
+					</p>
+					<p className="invoice-right">${cleanCost}</p>
+				</div>
+				<div className='invoice-row' id="service-cost">
+					<p className="invoice-left">
+						Service Fee
+					</p>
+					<p className="invoice-right">${servCost}</p>
+				</div>
+				<div className='invoice-row' id="total-cost">
+					<p className="invoice-left">
+						${nightly_rate} x {nightStay} nights
+					</p>
+					<p className="invoice-right">${totalCost}</p>
+				</div>
+			</div>
 		</div>
 	);
 };
