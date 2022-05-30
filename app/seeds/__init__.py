d711a752abe470e88f6150b330462aeb67b0d4ed
Brian May <brianmay2014@gmail.com>
2022-05-29 17:13:23 -0700

    seeded images complete

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -3,6 +3,7 @@ from .users import seed_users, undo_users
 from .estate_types import seed_estate_types, undo_estate_types
 from .estate import seed_estates, undo_estates
 from .critiques import seed_critiques, undo_critiques
+from .estate_images import seed_estate_images, undo_estate_images
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -17,6 +18,8 @@ def seed():
     seed_estate_types()
     seed_estates()
     seed_critiques()
+    seed_estate_images()
+    
 
 
 # Creates the `flask seed undo` command
@@ -27,3 +30,4 @@ def undo():
     undo_estates()
     undo_estate_types()
     undo_critiques()
+    undo_estate_images()
