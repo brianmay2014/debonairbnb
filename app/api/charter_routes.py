c2236e4cc6d824c052f42337221fc664436184db
Paul-Eirik <pmelhus@gmail.com>
2022-05-31 15:31:21 -0400

    charters can now be added to our database

--- app/api/charter_routes.py
+++ app/api/charter_routes.py
@@ -4,9 +4,28 @@ from app.models import db, Charter
 from app.forms import CharterForm
 
 
+
 charter_routes = Blueprint('charters', __name__)
 
-@charter_routes.route('', methods=['POST'])
+
+@charter_routes.route('/', methods=['POST'])
 @login_required
-def charter():
-  print(request, "====================")
+def charter_form_submit():
+  form = CharterForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  params = {
+    "user_id": form.data['user_id'],
+    "estate_id": form.data['estate_id'],
+    "guest_num": form.data['guest_num'],
+    "start_date": form.data['start_date'],
+    "end_date": form.data['end_date']
+  }
+
+  if form.validate_on_submit():
+    charter = Charter(**params)
+    db.session.add(charter)
+    db.session.commit()
+    return charter.to_dict()
+  else:
+    print(form.errors, 'ERROR')
