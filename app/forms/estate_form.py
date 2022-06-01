f46a3c017994b2f6f282e445102e8e5c29a515f7
Brian May <brianmay2014@gmail.com>
2022-06-01 08:17:00 -0700

    create estate is working

--- app/forms/estate_form.py
+++ app/forms/estate_form.py
@@ -1,16 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField
 from wtforms.validators import DataRequired, ValidationError
-from app.models import Estate
+from app.models import db, Estate, EstateType
 from ..seeds.estate_types import estate_types
-
-
-# def user_exists(form, field):
-#     # Checking if user exists
-#     email = field.data
-#     user = User.query.filter(User.email == email).first()
-#     if user:
-#         raise ValidationError('Email address is already in use.')
 
 
 # def username_exists(form, field):
@@ -20,12 +12,20 @@ from ..seeds.estate_types import estate_types
 #     if user:
 #         raise ValidationError('Username is already in use.')
 
+#query all for estate types, choices are the tuples
+
+# test = Estate.query.all()
+# print('-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/')
+# print(types)
+# print('-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/')
+# test = EstateType()
 
 class EstateForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     address = StringField('address', validators=[DataRequired()])
-    nightlyRate = IntegerField('nightlyRate')
-    type = SelectField('type', choices=estate_types)
+    nightly_rate = IntegerField('nightly_rate')
+    type_id = IntegerField('type_id')
+    # type_id = SelectField('type_id', choices=[], validators=[DataRequired()])
     description = StringField('description')
-    ownerId = IntegerField('ownerId', validators=[DataRequired()])
+    owner_id = IntegerField('owner_id', validators=[DataRequired()])
 
