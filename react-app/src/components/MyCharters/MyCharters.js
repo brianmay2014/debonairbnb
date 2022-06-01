9074c934227f5be4569966ab68369d6bdc35d60d
Paul-Eirik <pmelhus@gmail.com>
2022-06-01 00:58:47 -0400

    last edit

---
+++ react-app/src/components/MyCharters/MyCharters.js
@@ -0,0 +1,32 @@
+import "./MyCharters.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { genCharters } from "../../store/charter";
+
+const MyCharters = () => {
+  const dispatch = useDispatch();
+  const charters = useSelector((state) => Object.values(state?.charters));
+  const sessionUser = useSelector((state) => Object.values(state.session));
+
+  const yourCharters = charters.filter(
+    (charter) => charter.userId === sessionUser.user?.id
+  );
+
+  return (
+    <>
+      {yourCharters.map((charter) => {
+        return (
+          <div>
+            <h1>
+              {charter.start_date} to {charter.end_date}
+            </h1>
+            <button onClick={handleEdit}>Edit</button>
+            <button onClick={handleDelete}>Delete</button>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default MyCharters;
