4799701c71ca1dbc1510cde5d44f9ad4e0a924aa
Kai Seward <kairality@gmail.com>
2022-06-01 16:17:16 -0700

    rebase party

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import "./index.css";
-import App from "./App";
-import configureStore from "./store";
-=======
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
-<<<<<<< HEAD
->>>>>>> add modal for deleting critiques
+
 import { ModalProvider } from "./context/Modal";
-=======
-import { ModalProvider } from "./context/Modal"
->>>>>>> edit and delete
+
 
 const store = configureStore();
 
