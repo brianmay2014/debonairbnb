196a77835bdb9204f51a7aeeb78b7afe02ce955b
Brian May <brianmay2014@gmail.com>
2022-06-01 23:19:21 -0700

    update to fit 7 days on the calendar

--- react-app/src/components/estate/AvailabilityCal.js
+++ react-app/src/components/estate/AvailabilityCal.js
@@ -7,6 +7,8 @@ import "./estatePage.css";
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 
+import './CalendarFix.css';
+
 const AvailabilityCal = ({ estate, stateVars } ) => {
 	// const { id } = useParams();
 
