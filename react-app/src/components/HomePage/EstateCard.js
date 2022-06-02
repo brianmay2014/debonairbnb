064101b5546b1e8af9b5a3ef553a93aee21c01d0
Kai Seward <kairality@gmail.com>
2022-06-01 18:56:24 -0700

    cleanup

--- react-app/src/components/HomePage/EstateCard.js
+++ react-app/src/components/HomePage/EstateCard.js
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import RatingDisplay from "../estate/RatingDisplay";
 
 import "./HomePage.css";
 
-const EstateCard = ( { estate } ) => {
+const EstateCard = ({ estate }) => {
+  const [imageIndex, setImageIndex] = useState(0);
+  const [carouseling, setCarouseling] = useState(false);
+  const carouselLength = estate?.images?.length;
+  const img = estate?.images[imageIndex];
+  const src = img?.url;
+  const interval = useRef();
 
+  const address = `${estate?.city}, ${estate?.state}`;
 
-	const address = `${estate?.city}, ${estate?.state}`;
-    const dispImg = estate?.images[0]?.url;
+  useEffect(() => {
+    if (carouseling) {
+      interval.current = setInterval(() => {
+        setImageIndex((index) => (index + 1) % carouselLength);
+      }, 3000);
+    } else {
+      clearInterval(interval.current);
+    }
+    return () => clearInterval(interval.current);
+  }, [carouseling, carouselLength]);
 
-    // console.log(estate);
-
-	return (
-		<div className="estate-cards" id={`estate-card-${estate?.id}`}>
-			<div id="type-filter-bar"></div>
-			<div className="home-card-img" id="estate-display">
-				<a href={`/estates/${estate?.id}`}>
-                <img src={dispImg} alt="yup" />
-                </a>
-			</div>
-			<div className="home-card-text">
-				<div className="card-top-bar">
-					<div className="card-address">{address}</div>
-					<div className="card-critique">
-						<RatingDisplay rating={estate?.rating} places={2} />
-					</div>
-				</div>
-				<div className="card-availabilty">Feb 1-6</div>
-				<div className="card-cost">
-					${estate?.nightly_rate} <span className='per-night'>per night</span>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="estate-cards" id={`estate-card-${estate?.id}`}>
+      <div id="type-filter-bar"></div>
+      <div className="home-card-img" id="estate-display">
+        <a href={`/estates/${estate?.id}`}>
+          <img
+            src={src}
+            alt={img?.title}
+            onMouseEnter={() => setCarouseling(true)}
+            onMouseLeave={() => setCarouseling(false)}
+          />
+        </a>
+      </div>
+      <div className="home-card-text">
+        <div className="card-top-bar">
+          <div className="card-address">{address}</div>
+          <div className="card-critique">
+            <RatingDisplay rating={estate?.rating} places={2} />
+          </div>
+        </div>
+        <div className="card-availabilty">Feb 1-6</div>
+        <div className="card-cost">
+          ${estate?.nightly_rate} <span className="per-night">per night</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EstateCard;
