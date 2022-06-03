582f114db1edb1a7b9c7e66125e51fe0fd6438f2
Brian May <brianmay2014@gmail.com>
2022-06-03 10:17:48 -0700

    update airbnb icon to diamond, update creater charter page spacing

--- react-app/src/components/NavBar/NavIcon/NavIcon.js
+++ react-app/src/components/NavBar/NavIcon/NavIcon.js
@@ -4,10 +4,11 @@ const NavIcon = () => {
   return (
     <div className="nav-icon-div">
       <div id="icon-img-div">
-        <i class="fa-brands fa-airbnb fa-2xl" id="nav-icon"></i>
+        {"💎"}
+        {/* <i class="fa-brands fa-airbnb fa-2xl" id="nav-icon"></i> */}
       </div>
       <a id='header-homepage-link' href='/'>
-      <h2 id="nav-icon-title">Debonairbnb</h2>
+        <h2 id="nav-icon-title">Debonairbnb</h2>
       </a>
     </div>
   );
