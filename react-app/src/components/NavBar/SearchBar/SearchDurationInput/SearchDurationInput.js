3d6687b1a20519c56e4e13aa2cd93c86a8df2189
Paul-Eirik <pmelhus@gmail.com>
2022-06-03 02:34:50 -0400

    search function works with dates added as data point

--- react-app/src/components/NavBar/SearchBar/SearchDurationInput/SearchDurationInput.js
+++ react-app/src/components/NavBar/SearchBar/SearchDurationInput/SearchDurationInput.js
@@ -1,14 +1,115 @@
 import { addDays } from "date-fns";
-import React, { useEffect, useState } from "react";
 import { DateRangePicker } from "react-date-range";
+import { useEffect, useState } from "react";
+import "./SearchDurationInput.css";
+import { useSelector, useDispatch } from "react-redux";
 
-const SearchDurationInput = () => {
+const SearchDurationInput = ({setCheckoutDate, setCheckinDate, checkoutDate, checkinDate, setShowDateRange, showDateRange}) => {
 
+
+
+  const today = new Date();
+
+  const dateParser = (dateobj) => {
+    let year = dateobj.getFullYear();
+    let month = dateobj.getMonth() + 1;
+    let day = dateobj.getDate();
+
+    if (month < 10) {
+      month = `0${month.toString()}`;
+    }
+    if (day < 10) {
+      day = `0${day.toString()}`;
+    }
+
+    return `${year}-${month.toString()}-${day.toString()}`;
+  };
+
+  const [state, setState] = useState([
+    {
+      // startDate: new Date(),
+      // endDate: addDays(new Date(), 7),
+      // key: "selection",
+      startDate: today,
+      endDate: addDays(today, 1),
+      key: "selection",
+      color: "#c28849",
+    },
+  ]);
+
+  const setCheckoutFunc = async (e) => {
+    let strDate = e.target.value;
+
+    let newDate = new Date(strDate);
+    let setDate = addDays(newDate, 1);
+    setCheckoutDate(setDate);
+  };
+
+  const setCheckinFunc = async (e) => {
+    let strDate = e.target.value;
+    // console.log('-*/-*/-*/-*/-*/-*/-CHECKIN FUNC*/-*/-*/-*/-*/-*/-*/-*/-*/-*/');
+    // console.log("strDate", strDate);
+    let newDate = new Date(strDate);
+    // console.log("newDate", newDate);
+
+    let setDate = addDays(newDate, 1);
+    setCheckinDate(setDate);
+    // console.log('CHECKIN DATE', checkinDate);
+    // console.log("-*/-*/-*/-*/-*/-*/-*CHECKINFUNC/-*/-*/-*/-*/-*/-*/-*/-*/-*/");
+    // setCheckinDate(e.target.value)
+    // setNightStay(getNightStay(checkinDate, checkoutDate));
+  };
+
+  useEffect(() => {
+    const unHideCalendar = () => {
+      setShowDateRange(false);
+    };
+
+    document.addEventListener("click", unHideCalendar);
+    return () => document.removeEventListener("click", unHideCalendar);
+  }, [showDateRange]);
+
+  useEffect(() => {
+		setCheckinDate(state[0].startDate)
+		setCheckoutDate(state[0].endDate)
+		// console.log(disabledDatesArray)
+	}, [state])
 
   return (
-    <div>
-      <input type='date' placeholder='check-in'/>
-      <input type='date' placeholder='check-out'/>
+    <div className="checkin-checkout-inputs">
+      <div className="checkdates-2">
+        <label>Check-in</label>
+        <input
+          type="date"
+          // value={checkinDate}
+          value={dateParser(checkinDate)}
+          required
+          onChange={setCheckinFunc}
+          onClick={() => setShowDateRange(true)}
+        />
+      </div>
+      <div className="checkdates-2">
+        <label>Check-out</label>
+        <input
+          type="date"
+          value={dateParser(checkoutDate)}
+          required
+          onChange={setCheckoutFunc}
+        />
+      </div>
+      {showDateRange && (
+        <div className="search-date-range">
+          <DateRangePicker
+            onChange={(item) => setState([item.selection])}
+            showSelectionPreview={true}
+            moveRangeOnFirstSelection={false}
+            months={2}
+            ranges={state}
+            direction="horizontal"
+            dragSelectionEnabled={true}
+          />
+        </div>
+      )}
     </div>
   );
 };
