ce2a77733e1988d9b5b808f8d609b41d9d2fa28a
Brian May <brianmay2014@gmail.com>
2022-06-02 21:59:39 -0700

    footer and home page centering

---
+++ react-app/src/components/HomePage/Footer.js
@@ -0,0 +1,62 @@
+function Footer() {
+	return (
+		<div className="footer-backer">
+			<div className="footer">
+				<span>© 2022 Debonairbnb</span>
+				<div>
+					<a
+						href="https://github.com/brianmay2014"
+						rel="noreferrer"
+						target="_blank"
+					>
+						Github
+					</a>
+					<span className='footer-names'>{" • Brian May • "}</span>
+					<a
+						href="http://www.linkedin.com/in/brian-may-6aa49172"
+						rel="noreferrer"
+						target="_blank"
+					>
+						LinkedIn
+					</a>
+				</div>
+				<div>
+					<a
+						href="https://github.com/kairality"
+						rel="noreferrer"
+						target="_blank"
+					>
+						Github
+					</a>
+					<span className='footer-names'>{" • Kai Seward • "}</span>
+					<a
+						href="https://www.linkedin.com/in/kai-seward-01338b95/"
+						rel="noreferrer"
+						target="_blank"
+					>
+						LinkedIn
+					</a>
+				</div>
+				<div>
+					<a
+						href="https://github.com/pmelhus"
+						rel="noreferrer"
+						target="_blank"
+					>
+						Github
+					</a>
+					<span className='footer-names'>{" • Paul Melhus • "}</span>
+					<a
+						href="https://www.linkedin.com/in/paulmelhus/"
+						rel="noreferrer"
+						target="_blank"
+					>
+						LinkedIn
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
