d56b55f6b907e7e2ac50c18b4e7c8a75acd85785
Brian May <brianmay2014@gmail.com>
2022-06-02 20:00:18 -0700

    login form is done

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,51 +1,56 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { login } from '../../store/session';
-import './AuthForms.css';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { login } from "../../store/session";
+import "./AuthForms.css";
 
 const LoginForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+	const [errors, setErrors] = useState([]);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const user = useSelector((state) => state.session.user);
+	const dispatch = useDispatch();
 
-  const onLogin = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login(email, password));
-    if (data) {
-      setErrors(data);
-    }
-  };
+	const onLogin = async (e) => {
+		e.preventDefault();
+		const data = await dispatch(login(email, password));
+		if (data) {
+			setErrors(data);
+		}
+	};
 
-  const demoSubmit = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login('demo@demo.com', 'password'));
-    if (data) {
-      setErrors(data);
-    }
-  };
+	const demoSubmit = async (e) => {
+		e.preventDefault();
+		const data = await dispatch(login("demo@demo.com", "password"));
+		if (data) {
+			setErrors(data);
+		}
+	};
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
+	const toSignup = async (e) => {
+		e.preventDefault();
+		return <Redirect to="/signup" />;
+	};
 
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
+	const updateEmail = (e) => {
+		setEmail(e.target.value);
+	};
 
-  if (user) {
-    return <Redirect to='/' />;
-  }
+	const updatePassword = (e) => {
+		setPassword(e.target.value);
+	};
 
-  return (
+	if (user) {
+		return <Redirect to="/" />;
+	}
+
+	return (
 		<form className="auth-form" onSubmit={onLogin}>
-			<div className='login-text'>
-				<h3>Debonairbnb is an exclusive platform</h3>
-				<h4>you must log in to continue.</h4>
+			<div className="login-text">
+				<h3 id="h3-login">Debonairbnb is an exclusive platform</h3>
+				<h4 id="h4-login">you must log in to continue.</h4>
 			</div>
-			<div>
+			<div className="form-errors center">
 				{errors.map((error, ind) => (
 					<div key={ind}>{error}</div>
 				))}
@@ -70,16 +75,22 @@ const LoginForm = () => {
 					onChange={updatePassword}
 				/>
 				<div>
+					<div className='login-buttons-containers'>
 					<button className="btn" type="submit">
 						Login
 					</button>
-          <button className='btn-cancel' onClick={demoSubmit}>
-            Demo Login
-          </button>
+					<button className="btn-cancel" onClick={demoSubmit}>
+						Demo Login
+					</button>
+					</div>
+					<p>Don't have an account?</p>
+					<button id="to-sign-up" className="btn-pink" onClick={toSignup}>
+						Sign up!
+					</button>
 				</div>
 			</div>
 		</form>
-  );
+	);
 };
 
 export default LoginForm;
