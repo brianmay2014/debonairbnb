230536bfe5872f8ecc0171550ad826c0b30a326b
Brian May <brianmay2014@gmail.com>
2022-06-03 17:42:04 -0700

    added page if user has no charters

--- react-app/src/components/Charters/MyCharters/MyCharters.js
+++ react-app/src/components/Charters/MyCharters/MyCharters.js
@@ -18,18 +18,51 @@ const MyCharters = () => {
 
   // const [showCharterModal, setShowCharterModal] = useState(false);
 
-  return (
-    <div className="my-charters-container">
-      <h1>My Charters</h1>
-      {yourCharters.map((charter) => {
-        return (
-          <div>
-            <CharterRow charter={charter} estateId={charter.estate_id} />
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (yourCharters.length) {
+    return (
+		<div className="my-charters-container">
+			<h1>My Charters</h1>
+			{yourCharters.map((charter) => {
+				return (
+					<div>
+						<CharterRow
+							charter={charter}
+							estateId={charter.estate_id}
+						/>
+					</div>
+				);
+			})}
+		</div>
+	);
+  } else {
+    return (
+		<div className="my-charters-container">
+			<h1>My Charters</h1>
+			<div className='no-charter-header'>
+					<h2>We don't see any charters listed from your account.</h2>
+					<h3>
+						Browse the estates collection to find your next dream charter.
+					</h3>
+				</div>
+		</div>
+	);
+  }
+
+
+
+
+//   return (
+//     <div className="my-charters-container">
+//       <h1>My Charters</h1>
+//       {yourCharters.map((charter) => {
+//         return (
+//           <div>
+//             <CharterRow charter={charter} estateId={charter.estate_id} />
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
 };
 
 export default MyCharters;
